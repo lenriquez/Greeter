4181743844f7e56334b015598664b52da55c8e9c
lenriquez <felipe096@gmail.com>
2016-09-19 12:31:58 -0500

    Add init function to make sure that is possible to create object with out the word 'new'

--- Greetr.js
+++ Greetr.js
@@ -1,5 +1,15 @@
 
 (function(global, $){
 
+  var Greetr = function(firstName, lastName, language){
+    return new Greetr.init(firstName. lastName, language);
+  }
+
+  Greetr.init = function(firstName, lastName, language){
+    var self = this;
+    self.firstName = firstName || '';
+    self.lastName  = lastName  || '' ;
+    self.language  = language  || 'en'
+  }
  
 }(window, jQuery));
