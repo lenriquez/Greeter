4b3a15c928ee53d69466321290665f19e6c64794
lenriquez <felipe096@gmail.com>
2016-09-19 12:40:11 -0500

    Adding some comments

--- Greetr.js
+++ Greetr.js
@@ -1,6 +1,7 @@
 
 (function(global, $){
-
+ 
+  // Private variables
   var supportedLangs = ['en','es']; 
   var greetings = { 
     en: 'Hello',
@@ -17,17 +18,18 @@
     es: 'Inició sesión'
   }
 
+  // Construture 
   var Greetr = function(firstName, lastName, language){
     return new Greetr.init(firstName. lastName, language);
   }
 
+  // Object Constructure
   Greetr.init = function(firstName, lastName, language){
     var self = this;
     self.firstName = firstName || '';
     self.lastName  = lastName  || '' ;
     self.language  = language  || 'en'
   }
-
 
   global.Greetr = global.G$ = Greetr;
  
